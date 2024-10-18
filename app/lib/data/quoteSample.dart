5c3d59df0f2a9cb811ecb102c07d9288bfa6548c
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-18 04:15:50 +0300

    added assets for quote

--- app/lib/data/quoteSample.dart
+++ app/lib/data/quoteSample.dart
@@ -1,8 +1,8 @@
 import '../models/quote.dart';
 List<Quote> sampleQuotes = [
-  Quote(id: 1, quoteImage: "quote1.png", saintId: 1, quoteContent: "Live with love", favoriteCount: 120),
-  Quote(id: 2, quoteImage: "quote2.png", saintId: 2, quoteContent: "Faith conquers all", favoriteCount: 98),
-  Quote(id: 3, quoteImage: "quote3.png", saintId: 1, quoteContent: "Patience is strength", favoriteCount: 150),
-  Quote(id: 4, quoteImage: "quote4.png", saintId: 3, quoteContent: "Serve with joy", favoriteCount: 75),
-  Quote(id: 5, quoteImage: "quote5.png", saintId: 4, quoteContent: "Humility in service", favoriteCount: 45),
+  Quote(id: 1, quoteImage: "assets/quoteSample/quote.png", saintId: 1, quoteContent: "Live with love", favoriteCount: 120),
+  Quote(id: 2, quoteImage: "assets/quoteSample/quote2.png", saintId: 2, quoteContent: "Faith conquers all", favoriteCount: 98),
+  Quote(id: 3, quoteImage: "assets/quoteSample/quote.png", saintId: 1, quoteContent: "Patience is strength", favoriteCount: 150),
+  Quote(id: 4, quoteImage: "assets/quoteSample/quote2.png", saintId: 3, quoteContent: "Serve with joy", favoriteCount: 75),
+  Quote(id: 5, quoteImage: "assets/quoteSample/quote.png", saintId: 4, quoteContent: "Humility in service", favoriteCount: 45),
 ];
