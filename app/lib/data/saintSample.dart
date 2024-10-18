d92af86373b3740bb147d8976f506d1169952990
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-18 03:50:16 +0300

    added saint sample

---
+++ app/lib/data/saintSample.dart
@@ -0,0 +1,8 @@
+import '../models/saint.dart';
+List<Saint> sampleSaints = [
+  Saint(id: 1, storyIdList: [1, 2], quoteIdList: [1, 3], name: "Saint John"),
+  Saint(id: 2, storyIdList: [3], quoteIdList: [2], name: "Saint Mary"),
+  Saint(id: 3, storyIdList: [4, 5], quoteIdList: [4], name: "Saint Paul"),
+  Saint(id: 4, storyIdList: [2], quoteIdList: [5], name: "Saint Peter"),
+  Saint(id: 5, storyIdList: [1, 5], quoteIdList: [1, 2], name: "Saint James"),
+];
