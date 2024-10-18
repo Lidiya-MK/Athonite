d0d236c30f5ae6d8b82a19f0380361d7a26a5bc8
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-18 04:07:37 +0300

    added assets for category

--- app/lib/data/categorySample.dart
+++ app/lib/data/categorySample.dart
@@ -1,8 +1,8 @@
 import '../models/category.dart';
 List<Category> sampleCategories = [
-  Category(id: 1, storyIdList: [1, 3], image: "category1.png", name: "Miracles"),
-  Category(id: 2, storyIdList: [2, 5], image: "category2.png", name: "Journeys"),
-  Category(id: 3, storyIdList: [4], image: "category3.png", name: "Conversions"),
-  Category(id: 4, storyIdList: [], image: "category4.png", name: "Teachings"),
-  Category(id: 5, storyIdList: [], image: "category5.png", name: "Leadership"),
+  Category(id: 1, storyIdList: [1, 3], image: "assets/categorySample/counsel.png", name: "Miracles"),
+  Category(id: 2, storyIdList: [2, 5], image: "assets/categorySample/bio.png", name: "Journeys"),
+  Category(id: 3, storyIdList: [4], image: "assets/categorySample/counsel.png", name: "Conversions"),
+  Category(id: 4, storyIdList: [], image: "assets/categorySample/bio.png", name: "Teachings"),
+  Category(id: 5, storyIdList: [], image: "assets/categorySample/counsel.png", name: "Leadership"),
 ];
