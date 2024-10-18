7f333890b9df07f7fc1c7c2a9b6208919aa12349
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-18 03:52:01 +0300

    added story sample

---
+++ app/lib/data/storySample.dart
@@ -0,0 +1,8 @@
+import '../models/story.dart';
+List<Story> sampleStories = [
+  Story(id: 1, favoriteCount: 200, storyContent: "Story of Saint John’s miracles", storyTitle: "The Miracles of Saint John", storyImage: "story1.png", categoryId: 1),
+  Story(id: 2, favoriteCount: 180, storyContent: "Story of Saint Peter's journey", storyTitle: "The Journey of Saint Peter", storyImage: "story2.png", categoryId: 2),
+  Story(id: 3, favoriteCount: 250, storyContent: "Story of Saint Mary’s devotion", storyTitle: "The Devotion of Saint Mary", storyImage: "story3.png", categoryId: 1),
+  Story(id: 4, favoriteCount: 90, storyContent: "Story of Saint Paul’s conversion", storyTitle: "Conversion of Saint Paul", storyImage: "story4.png", categoryId: 3),
+  Story(id: 5, favoriteCount: 110, storyContent: "Story of Saint James’ leadership", storyTitle: "Leadership of Saint James", storyImage: "story5.png", categoryId: 2),
+];
