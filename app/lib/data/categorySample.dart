c4a7aa9266790f8ac6ac10b21fcb82b63a2f4ea7
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-18 03:52:47 +0300

    added category sample

---
+++ app/lib/data/categorySample.dart
@@ -0,0 +1,8 @@
+import '../models/category.dart';
+List<Category> sampleCategories = [
+  Category(id: 1, storyIdList: [1, 3], image: "category1.png", name: "Miracles"),
+  Category(id: 2, storyIdList: [2, 5], image: "category2.png", name: "Journeys"),
+  Category(id: 3, storyIdList: [4], image: "category3.png", name: "Conversions"),
+  Category(id: 4, storyIdList: [], image: "category4.png", name: "Teachings"),
+  Category(id: 5, storyIdList: [], image: "category5.png", name: "Leadership"),
+];
