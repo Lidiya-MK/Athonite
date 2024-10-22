1b27ab1a7e1e20b800961c977f5f9884eef699d2
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-22 15:35:53 +0300

    added top section for quotes screen to display logo and userID

--- app/lib/presentation/screens/quotes.dart
+++ app/lib/presentation/screens/quotes.dart
@@ -4,12 +4,16 @@ import '../../models/quote.dart';
 import '../../data/quoteSample.dart';
 import '../screens/favorites.dart';
 import '../screens/home.dart';
+import '../../data/userSample.dart';
+import '../../models/user.dart';
 
 class QuotesScreen extends StatelessWidget {
   const QuotesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    User currentUser = sampleUsers[0];
+
     return Scaffold(
       body: Stack(
         children: [
@@ -27,9 +31,48 @@ class QuotesScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Athonite",
+                      style: GoogleFonts.islandMoments(
+                        color: const Color(0xFFFFD700),
+                        fontSize: 40,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage(currentUser.profilePic),
+                              radius: 16.5,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              currentUser.username,
+                              style: const TextStyle(
+                                color: Color(0xFFCDCBCB),
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 8),
+                        const Icon(
+                          Icons.logout,
+                          color: Color(0xFFCDCBCB),
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+           
                 Center(
                   child: Text(
                     "Quotes",
@@ -39,7 +82,7 @@ class QuotesScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 10),
                 Expanded(
                   child: SingleChildScrollView(
                     physics: const BouncingScrollPhysics(),
@@ -94,45 +137,45 @@ class QuotesScreen extends StatelessWidget {
           ),
         ],
       ),
-     bottomNavigationBar: BottomNavigationBar(
-  backgroundColor: Colors.black,
-  items: const [
-    BottomNavigationBarItem(
-      icon: Icon(Icons.home, color: Color(0xFFDACFB1)),
-      label: 'Home',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.favorite, color: Color(0xFF898484)),
-      label: 'Favorites',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.bookmark, color: Color(0xFF898484)),
-      label: 'Quotes',
-    ),
-  ],
-  currentIndex: 0, 
-  selectedItemColor: const Color(0xFFDACFB1),
-  unselectedItemColor: const Color(0xFF898484),
-  showUnselectedLabels: true,
-  onTap: (index) {
-    if (index == 0) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
-      );
-    } else if (index == 1) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const FavoritesScreen()),
-      );
-    } else if (index == 2) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const QuotesScreen()), 
-      );
-    }
-  },
-),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Color(0xFFDACFB1)),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite, color: Color(0xFF898484)),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark, color: Color(0xFF898484)),
+            label: 'Quotes',
+          ),
+        ],
+        currentIndex: 2, 
+        selectedItemColor: const Color(0xFFDACFB1),
+        unselectedItemColor: const Color(0xFF898484),
+        showUnselectedLabels: true,
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+            );
+          } else if (index == 1) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const FavoritesScreen()),
+            );
+          } else if (index == 2) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const QuotesScreen()), 
+            );
+          }
+        },
+      ),
     );
   }
 }
